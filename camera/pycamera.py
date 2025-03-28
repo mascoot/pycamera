d7fafb81fbfbc3dd9ed00433fd6dc99bde792441
mascoot <james_92one@hotmail.com>
2025-03-28 12:39:28 +0800

    Add Scroll bar to zoom image

--- camera/pycamera.py
+++ camera/pycamera.py
@@ -3,7 +3,7 @@ import numpy as np
 import threading
 
 class pyCamera:
-  def __init__(self, camera_index = 0, start_capture=True):
+  def __init__(self, camera_index:int = 0, start_capture:bool=True) -> None:
     self.init = False  # Ensure init flag is set initially
     self.lock = threading.Lock()  # Thread lock for shared resources
         
@@ -26,59 +26,60 @@ class pyCamera:
     self.frame_height = self.vid.get(cv.CAP_PROP_FRAME_HEIGHT)
     self.video_fps = self.vid.get(cv.CAP_PROP_FPS)
     
-    self.is_active = start_capture
-    self.framebuffer = [None, None]    
-    self.cFrame = 0
+    self.is_active:bool = start_capture
+    self.framebuffer = [None, None]  # Initialize framebuffer with None
+    self.cFrame:int = 0
     self.capture_thread = None  # Initialize thread variable
     self.init = True  # Mark initialization as successful
     
     if self.is_active:
         self.start_capture()
   
-  def __del__(self):
+  def __del__(self) -> None:
     if self.init:
       self.stop_capture()
       if self.capture_thread and self.capture_thread.is_alive():
           self.capture_thread.join()
       self.vid.release()
     
-  def get_frame_size(self):
-    return (self.frame_width, self.frame_height) if self.init else (0, 0)
+  def get_frame_size(self) -> tuple[int, int]:
+    return (int(self.frame_width), int(self.frame_height)) if self.init else (0, 0)
   
-  def get_fps(self):
-    return self.video_fps if self.init else 0
+  def get_fps(self) -> float:
+    return self.video_fps if self.init else 0.0
   
-  def get_framebuffer(self):
+  def get_framebuffer(self) -> np.ndarray|None:
     if not self.init:
       return None
     with self.lock:
       return self.framebuffer[self.cFrame]
 
-  def start_capture(self):
+  def start_capture(self) -> bool:
     if not self.init or (self.capture_thread and self.capture_thread.is_alive()):
-        return
+        return False
     
     self.is_active = True
     self.capture_thread = threading.Thread(target=self.__capture_loop, daemon=True)
     self.capture_thread.start()
+    return True
     
-  def stop_capture(self):
+  def stop_capture(self) -> None:
     self.is_active = False
     if self.capture_thread and self.capture_thread.is_alive():
         self.capture_thread.join()
     
-  def set_capture(self, capture_flag):
+  def set_capture(self, capture_flag) -> None:
     self.is_active = capture_flag
   
-  def __capture_loop(self):
+  def __capture_loop(self) -> None:
     while self.is_active:
       self.__capture_frame()
   
-  def __capture_frame(self):
+  def __capture_frame(self) -> bool:
     is_valid_frame, frame = self.vid.read()
     if not is_valid_frame or frame is None:
       print("Frame not valid")
-      return
+      return False
     
     frame_texture = self.__prepare_cv_framebuffer(frame)
     with self.lock:
@@ -86,8 +87,10 @@ class pyCamera:
       self.cFrame = (self.cFrame + 1) % len(self.framebuffer)
       #update current framebuffer texture
       self.framebuffer[self.cFrame] = frame_texture
+      
+    return True
   
-  def __prepare_cv_framebuffer(self, frame):
+  def __prepare_cv_framebuffer(self, frame) -> np.ndarray[tuple[int], np.dtype[np.float32]]:
       data = np.flip(frame, 2)  # Convert BGR to RGB
       data = data.astype(np.float32) / 255.0  # Normalize and convert to float32
       return data.ravel()
--- main.py
+++ main.py
@@ -1,28 +1,47 @@
 from camera import pycamera as pyc
 import dearpygui.dearpygui as dpg
-import threading
+
+VIEWPORT_WIDTH = 1280
+VIEWPORT_HEIGHT = 720
+IMGUI_DEFAULT_PADDING = 20
 
 def save_callback():
       print("Save Clicked")
-      
+
+def image_resize(sender, app_data, user_data):
+      img_scale = app_data
+      offset = int((VIEWPORT_WIDTH - int(VIEWPORT_WIDTH * img_scale))*0.5) - IMGUI_DEFAULT_PADDING
+      scaled_width = int(VIEWPORT_WIDTH * img_scale)
+      scaled_height = int(VIEWPORT_HEIGHT * img_scale)
+      dpg.set_item_width("img_frame", scaled_width)
+      dpg.set_item_height("img_frame", scaled_height)
+      dpg.configure_item("img_frame", width=scaled_width, height=scaled_height, indent=offset)
+  
 if __name__ == "__main__":
   
   camera = pyc.pyCamera(start_capture=True)
   
   dpg.create_context()
-  dpg.create_viewport(title='pyCameraCalibration', width=1280, height=720)
+  dpg.create_viewport(title='pyCameraCalibration', width=VIEWPORT_WIDTH, height=VIEWPORT_HEIGHT)
   dpg.setup_dearpygui()
   
   texture_data = camera.get_framebuffer()
   frame_size = camera.get_frame_size()
-  dummy_data = [255] * int(frame_size[1] * frame_size[0])
+  dummy_data = [255.0] * int(frame_size[1] * frame_size[0])
 
   with dpg.texture_registry(show=False):
     dpg.add_raw_texture(frame_size[0], frame_size[1], dummy_data, tag="cvFramebuffer", format=dpg.mvFormat_Float_rgb)
-    
-  with dpg.window(label="Example Window"):
-    dpg.add_image("cvFramebuffer")
-   
+  
+  img_scale = 0.8  
+  offset = int((VIEWPORT_WIDTH - int(VIEWPORT_WIDTH * img_scale))*0.5) - IMGUI_DEFAULT_PADDING
+  scaled_width = int(VIEWPORT_WIDTH * img_scale)
+  scaled_height = int(VIEWPORT_HEIGHT * img_scale)
+  with dpg.window(label="Example Window", tag="main_window"):
+    dpg.add_text("pyCamera Calibration")
+    dpg.add_slider_float(label="Zoom", default_value=img_scale, min_value=0.1, max_value=2.0, callback=image_resize)
+    dpg.add_image("cvFramebuffer", tag="img_frame", width=scaled_width, height=scaled_height, indent=offset)
+  
+  dpg.set_primary_window("main_window", True)
   dpg.show_metrics() 
   dpg.show_viewport()
   while dpg.is_dearpygui_running():
